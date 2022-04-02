249ba5dad6e936b3dac7c362a4d333d4c4efafd4
Ezra Pool <ezra@digitalnatives.nl>
2022-04-02 17:04:55 +0200

    feat: fix zoom issue and automatically zoom to your current location

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,5 +1,8 @@
 <template>
     <l-map
+        @ready="onReady"
+        @locationfound="onLocationFound"
+        ref="map"
         class="map"
         :zoom="zoom"
         :center="center"
@@ -11,35 +14,57 @@
             :url="url"
             :attribution="attribution"
             layer-type="base"
+            :max-zoom="options.maxZoom"
+            :min-zoom="options.minZoom"
         />
+
+        <template v-if="location">
+            <l-circle-marker :lat-lng="location.latlng" :fillOpacity="1" :radius="0.1" />
+            <l-circle-marker :lat-lng="location.latlng" :radius="location.accuracy / 2" :stroke="false" />
+        </template>
     </l-map>
 </template>
 
 <script>
 import "leaflet/dist/leaflet.css";
-import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
+import { LMap, LTileLayer, LCircleMarker } from "@vue-leaflet/vue-leaflet";
 
 export default  {
     components: {
         LMap,
         LTileLayer,
+        LCircleMarker
     },
     data() {
         return {
+            location: null,
             zoom: 13.2,
             center: [52.375103, 4.890201],
             bounds: null,
             url: 'https://maps.tsdme.workers.dev/{z}/{x}/{y}.png',
             // url: 'https://tile.thesonicdeathmonkeyexperience.nl/{z}/{x}/{y}.png',
             attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors',
-            currentZoom: 15,
+            currentZoom: 14,
             currentCenter: [52.375103, 4.890201],
             options: {
                 zoomSnap: 0.5,
-                maxZoom: 15
+                maxZoom: 19,
+                minZoom: 1
             }
         };
     },
+    methods: {
+        onReady() {
+            this?.$refs?.map?.leafletObject?.locate({
+                maximumAge: 86400,
+                enableHighAccuracy: true,
+                setView: true
+            });
+        },
+        onLocationFound(location) {
+            this.location = location;
+        }
+    }
 }
 </script>
 
